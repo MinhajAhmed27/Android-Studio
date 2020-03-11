65216472e74cc28854fc49cdd899c07492593806
MinhajAhmed27 <60568039+MinhajAhmed27@users.noreply.github.com>
2020-03-11 20:50:20 +0500

    Using Explicit Intent to navigate b/w Activities

--- app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myfirstandroidapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,10 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        buttonEnter.setOnClickListener {
+        btnSend.setOnClickListener {
 
             Log.i("MainActivity", "button was clicked" )
-            Toast.makeText(this,"button was clicked",Toast.LENGTH_SHORT).show()
+
+            val message = PlainText.text.toString()
+
+            Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
